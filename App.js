f41686b1eb7130292396d6361af80bc7fdc7bf3b
Sven Raskin <sven.raskin@me.com>
2024-03-08 17:50:38 +0100

    AboutScreen PoC mit Theme

--- App.js
+++ App.js
@@ -1,32 +1,69 @@
-import { Button } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { AboutScreen } from "./screens/about";
-import { HomeScreen } from "./screens/home";
+import React from 'react';
+import { Button, Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AboutScreen } from './screens/about';
+import { HomeScreen } from './screens/home';
+import { colors} from './theme';
+import { fonts } from './theme';
 
 function App() {
-	const Stack = createNativeStackNavigator();
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen
-					name="Home"
-					component={HomeScreen}
-					options={({ navigation, route }) => ({
-						headerTitle: "Ladefuchs",
-						// Add a placeholder button without the `onPress` to avoid flicker
-						headerRight: () => (
-							<Button
-								title="About"
-								onPress={() => navigation.navigate("About")}
-							/>
-						),
-					})}
-				/>
-				<Stack.Screen name="About" component={AboutScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation, route }) => ({
+            headerTitle: () => (
+              <View style={styles.headerTitleContainer}>
+                <Image
+                  source={require('./assets/ladefuchs.png')} // Passen Sie den Pfad zum Bild an
+                  style={styles.headerImage}
+                />
+                
+              </View>
+            ),
+            headerRight: () => (
+				<TouchableOpacity onPress={() => navigation.navigate('About')}>
+				  <Image
+					source={require('./assets/zahnrad.png')}
+					style={{ width: 32, height: 32 }}
+				  />
+				</TouchableOpacity>
+			  ),
+            headerStyle: {
+              backgroundColor: colors.background, // Verwendung der Ladefuchs Farbe für den Header-Hintergrund
+            },
+            headerTintColor: colors.ladefuchsOrange, // Farbe für den Header-Text
+          })}
+        />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+const styles = StyleSheet.create({
+  headerTitleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerImage: {
+    width: 62,
+    height: 62,
+    marginRight: 0,
+	marginTop: -11,
+  },
+  headerTitle: {
+    color: colors.ladefuchsOrange,
+    fontSize: 18,
+  },
+})
+
 
 export default App;
--- screens/home.tsx
+++ screens/home.tsx
@@ -1,16 +1,21 @@
-import { View, Text, Button } from "react-native";
+import React from "react";
+import { View, Text, Button, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { colors } from '../theme';
+
 
 export function HomeScreen(props) {
-	const navigation = useNavigation();
-	return (
-		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-			<Text>Home Screen 123</Text>
-			<Button
-				title="Go to About"
-				onPress={() => navigation.navigate("About")}
-			/>
-			<Text>Props: {JSON.stringify(props)}</Text>
-		</View>
-	);
+  const navigation = useNavigation();
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: colors.background }}>
+      {/* Anstatt des Textes wird hier das runde Bild eingefügt */}
+      <Image
+        source={require("../assets/LFfake.jpg")} // Pfad zum Bild anpassen
+        style={{ width: 380, height: 735 }} // Stil des Bildes anpassen, um es rund zu machen
+      />
+      <Text style={{ color: colors.ladefuchsOrange }}>Hier steht der Ladefuchs</Text>
+
+      {/*<Text>Props: {JSON.stringify(props)}</Text>*/}
+    </View>
+  );
 }
