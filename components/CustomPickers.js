bc015037422bcb55e6b985601b7566054cec6766
Sven Raskin <sven.raskin@me.com>
2024-04-05 15:48:47 +0200

    API V3 für Operator implementiert

--- components/CustomPickers.js
+++ components/CustomPickers.js
@@ -1,44 +1,49 @@
-import React, { useState } from 'react';
-import {Picker} from '@react-native-picker/picker';
+import React, { useState, useEffect } from 'react';
+import { Picker } from '@react-native-picker/picker';
 
 const CustomPicker = ({ onSelect }) => {
+  const [operators, setOperators] = useState([]);
   const [selectedValue, setSelectedValue] = useState('');
+
+  useEffect(() => {
+    const fetchOperators = async () => {
+      try {
+        const token = '52549df1e0cda21772f34c6244c9cb7e2ab24ff4'; // Hier setzen Sie Ihren API-Token ein
+        const response = await fetch('https://api.ladefuchs.app/v3/operators?standard=false', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Failed to fetch operators');
+        }
+        const data = await response.json();
+        setOperators(data.operators); // Beachten Sie den Unterschied hier
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchOperators();
+  }, []);
+
+  if (!operators) {
+    return null;
+  }
 
   return (
     <Picker
       selectedValue={selectedValue}
       style={{ height: 50, width: '100%' }}
-	  itemStyle={{ fontSize: 20 }} // Hier ändern Sie die Höhe der Picker Items
+      itemStyle={{ fontSize: 20 }}
       onValueChange={(itemValue, itemIndex) => {
         setSelectedValue(itemValue);
-        onSelect(itemValue); // Die Anbieter werden vermutlich aus dem Backend geladen
+        onSelect(itemValue);
       }}
     >
-      <Picker.Item label="Aldi Süd" value="id1" />
-        <Picker.Item label="Allego" value="id2" />
-        <Picker.Item label="Aral pulse" value="id3" />
-        <Picker.Item label="be.ENERGISED" value="id5" />
-        <Picker.Item label="Berliner Stadtwerke" value="id6" />
-        <Picker.Item label="Chargepoint" value="id7" />
-        <Picker.Item label="Citywatt" value="id8" />
-        <Picker.Item label="Comfort Charge" value="id9" />
-        <Picker.Item label="Compleo / Innogy" value="id10" />
-        <Picker.Item label="Elli" value="id11" />
-        <Picker.Item label="EnBW" value="id4" />
-        <Picker.Item label="E.ON" value="id12" />
-        <Picker.Item label="EWE SWB" value="id13" />
-        <Picker.Item label="Fastnet" value="id14" />
-        <Picker.Item label="Ionity" value="id15" />
-        <Picker.Item label="Kaufland" value="id16" />
-        <Picker.Item label="Ladenetz" value="id17" />
-        <Picker.Item label="Ladeverbund+" value="id18" />
-        <Picker.Item label="Lichtblick (ChargeIT)" value="id19" />
-        <Picker.Item label="Lidl" value="id20" />
-        <Picker.Item label="Mer Germany" value="id21" />
-        <Picker.Item label="Pfalzwerke" value="id22" />
-        <Picker.Item label="Shell Recharge" value="id23" />
-        <Picker.Item label="Stromnetz Hamburg" value="id24" />
-        <Picker.Item label="TankE" value="id25" />
+      {operators.map(operator => (
+        <Picker.Item key={operator.identifier} label={operator.name} value={operator.identifier} />
+      ))}
     </Picker>
   );
 };
