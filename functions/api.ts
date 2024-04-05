6d021049617a31e6a296ae58a14d0941720bc7e3
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2024-04-05 18:43:04 +0200

    api error handling

--- functions/api.ts
+++ functions/api.ts
@@ -1,20 +1,20 @@
 import { Operator, OperatorsResponse } from "../types/operator";
 
 const apiPath = "https://api.ladefuchs.app";
-const apiToken = process.env.API_TOKEN;
+const authHeader = {
+	headers: {
+		Authorization: `Bearer ${process.env.API_TOKEN}`,
+	},
+};
 
 export async function fetchOperators({ standard = true }): Promise<Operator[]> {
 	try {
 		const response = await fetch(
 			`${apiPath}/v3/operators?standard=${standard}`,
-			{
-				headers: {
-					Authorization: `Bearer ${apiToken}`,
-				},
-			}
+			authHeader
 		);
 		if (!response.ok) {
-			throw new Error("Failed to fetch operators");
+			throw new Error(`Failed to fetch operators: ${response}`);
 		}
 		const data = (await response.json()) as OperatorsResponse;
 		return data.operators;
