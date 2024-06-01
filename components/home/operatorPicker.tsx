0f63ef1066c9d79a57595bfe2f6b55f2917f6813
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2024-06-01 12:37:53 +0200

    android special picker

--- components/home/operatorPicker.tsx
+++ components/home/operatorPicker.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Picker } from "@react-native-picker/picker";
+import { Picker as AndroidPicker } from "react-native-wheel-pick";
 import { useAppStore } from "../../state/state";
 import { useShallow } from "zustand/react/shallow";
+import { colors } from "../../theme";
+import { Platform } from "react-native";
 
 const OperatorPicker = () => {
 	const { operators, operatorId, setOperatorId } = useAppStore(
@@ -12,6 +15,35 @@ const OperatorPicker = () => {
 		}))
 	);
 
+	if (!operatorId) {
+		return <></>;
+	}
+
+	if (Platform.OS === "android") {
+		const selectedOperatorName =
+			operators.find((item) => item.identifier === operatorId) ??
+			operators[0] ??
+			null;
+		return (
+			<AndroidPicker
+				style={{
+					backgroundColor: colors.ladefuchsLightBackground,
+					width: "100%",
+					height: "130%",
+				}}
+				selectedValue={selectedOperatorName.name}
+				// textSize={21}
+				selectBackgroundColor="#8080801A" // support HEXA color Style (#rrggbbaa)
+				pickerData={operators.map((item) => item.name)}
+				onValueChange={(newOperatorName) => {
+					const foundOperator = operators.find(
+						(item) => item.name === newOperatorName
+					);
+					setOperatorId(foundOperator.identifier);
+				}}
+			/>
+		);
+	}
 	return (
 		<Picker
 			selectedValue={operatorId}
