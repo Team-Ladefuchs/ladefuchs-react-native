f71923908f4096b14f3c07c2029e30b3a8e30b1b
Sven Raskin <sven.raskin@me.com>
2024-06-15 12:44:39 +0200

    text description added detail view fallback for CPO image

--- components/detail/logos.tsx
+++ components/detail/logos.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
 import { authHeader } from "../../functions/api";
 import { CardImage } from "../cardImage";
 import { Tariff } from "../../types/tariff";
@@ -16,34 +16,53 @@ export function Logos({
 		: require("@assets/cpo_generic.png");
 
 	return (
-		<>
-			<View
+		<View
+			style={{
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "center",
+				alignItems: "center",
+				gap: 5,
+			}}
+		>
+			<CardImage
+				tariff={tariff}
+				width={88}
 				style={{
-					display: "flex",
-					flexDirection: "row",
-					justifyContent: "center",
-					alignItems: "center",
-					gap: 5,
+					transform: [{ rotate: "-15deg" }],
 				}}
-			>
-				<CardImage
-					tariff={tariff}
-					width={88}
-					style={{
-						transform: [{ rotate: "-15deg" }],
-					}}
-				/>
+			/>
+			<View style={{ position: "relative" }}>
 				<Image
 					source={operatorImage}
-					height={120}
-					width={180}
 					style={{
 						height: 120,
 						width: 180,
 						objectFit: "scale-down",
 					}}
 				/>
+				{/* Conditionally render the Text component over the image if operatorImageUrl is null */}
+				{!operatorImageUrl && (
+					<View
+						style={{
+							position: "absolute",
+							top: "50%",
+							left: "50%",
+							transform: [
+								{ translateX: -10 },
+								{ translateY: -10 },
+							],
+							backgroundColor: "rgba(0, 0, 0, 0.2)",
+							padding: 5,
+						}}
+					>
+						{/* hier dann String aus operatorURL */}
+						<Text style={{ color: "white", textAlign: "center" }}>
+							Operator
+						</Text>
+					</View>
+				)}
 			</View>
-		</>
+		</View>
 	);
 }
