09a91c93f7850f6c3c07fa0f01105464fe270712
maxxweb <sven.raskin@gmail.com>
2024-06-30 15:27:22 +0200

    design styles

--- screens/feedbackView.tsx
+++ screens/feedbackView.tsx
@@ -1,48 +1,91 @@
 import React, { useState } from "react";
-import { View, Text, Button } from "react-native";
+import {
+	View,
+	Text,
+	Button,
+	TextInput,
+	ScrollView,
+	KeyboardAvoidingView,
+	Platform,
+} from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { styles } from "../theme";
 import { AppLogo } from "../components/header/appLogo";
 
 function FeedbackView() {
-    const [futterAntwort, setFutterAntwort] = useState("");
-    const [preisAntwort, setPreisAntwort] = useState("");
-    const [tarifAntwort, setTarifAntwort] = useState("");
+	const [preisAntwort, setPreisAntwort] = useState("AC");
+	const [freitext, setFreitext] = useState("");
 
-    const handleSubmit = () => {
-        // Hier kannst du die Antworten verarbeiten oder senden
-        console.log("Futter Antwort:", futterAntwort);
-        console.log("Preis Antwort:", preisAntwort);
-        console.log("Tarif Antwort:", tarifAntwort);
-    };
+	const handleSubmit = () => {
+		console.log("Freitext:", freitext);
+		console.log("Preis Antwort:", preisAntwort);
+	};
 
-    return (
-        <View style={styles.headerView}>
-             <Text style={styles.headLine}>Hast Du Futter für den Fuchs?</Text>
-            <View style={{ height: "30%", justifyContent: "center", alignItems: "center" }}>
-                <AppLogo size={110} />
-            </View>
-			<View style={{ height: "10%", justifyContent: "center", alignItems: "center" }}>
-			<Text style={styles.headerText}>Sag uns was nicht stimmt!</Text>
-            </View>
+	return (
+		<KeyboardAvoidingView
+			style={styles.container}
+			behavior={Platform.OS === "ios" ? "padding" : "height"}
+		>
+			<View contentContainerStyle={styles.scrollContainer}>
+				<View style={styles.headerView}>
+					<Text style={styles.headLine}>
+						Hast Du Futter für den Fuchs?
+					</Text>
+					<View
+						style={{
+							height: "25%",
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<AppLogo size={110} />
+					</View>
+					<View
+						style={{
+							height: "5%",
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<Text style={styles.headerText}>
+							Sag uns was nicht stimmt!
+						</Text>
+					</View>
 
-
-
-            <Picker
-                selectedValue={preisAntwort}
-                style={styles.picker}
-                onValueChange={(itemValue) => setPreisAntwort(itemValue)}
-            >
-                <Picker.Item label="AC Preis" value="AC" />
-                <Picker.Item label="DC Preis" value="DC" />
-                <Picker.Item label="Blockiergebühr" value="Gebühr" />
-            </Picker>
-
-            <View style={styles.buttonContainer}>
-                <Button title="Senden" onPress={handleSubmit} />
-            </View>
-        </View>
-    );
+					<View>
+						<Picker
+							selectedValue={preisAntwort}
+							style={styles.picker}
+							onValueChange={(itemValue) =>
+								setPreisAntwort(itemValue)
+							}
+						>
+							<Picker.Item label="AC Preis" value="AC" />
+							<Picker.Item label="DC Preis" value="DC" />
+							<Picker.Item
+								label="Blockiergebühr"
+								value="Gebühr"
+							/>
+						</Picker>
+					</View>
+					<View>
+						<TextInput
+							style={styles.textInput}
+							placeholder="Willst Du dem Fuchs noch etwas flüstern? (max. 160 Zeichen)"
+							maxLength={160}
+							value={freitext}
+							onChangeText={setFreitext}
+							multiline
+							numberOfLines={4}
+						/>
+					</View>
+					<View style={styles.buttonContainer}>
+						<Button title="Senden" onPress={handleSubmit} />
+					</View>
+				</View>
+			</View>
+		</KeyboardAvoidingView>
+	);
 }
 
 export default FeedbackView;
