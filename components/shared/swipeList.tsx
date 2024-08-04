a459bf7a461a2fbd6e888cbb268361f8721bdc7d
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2024-08-04 21:31:26 +0200

    custom tariffs improve scroll performance

--- components/shared/swipeList.tsx
+++ components/shared/swipeList.tsx
@@ -29,124 +29,122 @@ interface Section<T> {
 
 const isDigit = /[0-9]/;
 
-export const SwipeList = forwardRef(
-	<T extends { identifier: string; name: string }>(
-		{ data, onRemove, onAdd, exists, renderItem, containerStyle }: Props<T>,
-		ref: React.Ref<any>,
-	) => {
-		const [currentOpenItem, setCurrentOpenItem] = useState<T | null>(null);
-		const editButtonSize = scale(22);
-		const sections: Section<T>[] = useMemo(() => {
-			const sectionMap = data.reduce((acc, item) => {
-				let firstLetter = item.name.charAt(0).toUpperCase();
-				if (isDigit.test(firstLetter)) {
-					firstLetter = "#";
-				}
-				if (!acc[firstLetter]) {
-					acc[firstLetter] = {
-						title: firstLetter,
-						data: [],
-					} satisfies Section<T>;
-				}
-				acc[firstLetter].data.push(item);
-				return acc;
-			}, {});
+export function SwipeList<T extends { identifier: string; name: string }>({
+	data,
+	onRemove,
+	onAdd,
+	exists,
+	renderItem,
+	containerStyle,
+}: Props<T>) {
+	const [currentOpenItem, setCurrentOpenItem] = useState<T | null>(null);
+	const editButtonSize = scale(22);
+	const sections: Section<T>[] = useMemo(() => {
+		const sectionMap = data.reduce((acc, item) => {
+			let firstLetter = item.name.charAt(0).toUpperCase();
+			if (isDigit.test(firstLetter)) {
+				firstLetter = "#";
+			}
+			if (!acc[firstLetter]) {
+				acc[firstLetter] = {
+					title: firstLetter,
+					data: [],
+				} satisfies Section<T>;
+			}
+			acc[firstLetter].data.push(item);
+			return acc;
+		}, {});
 
-			return Object.values(sectionMap);
-		}, [data]);
+		return Object.values(sectionMap);
+	}, [data]);
 
-		// Define the height of each item (assuming all items have the same height)
-		const getItemLayout = (_: any, index: number) => ({
-			length: scale(86), // Adjust this to the actual item height
-			offset: scale(86) * index,
-			index,
-		});
+	// Define the height of each item (assuming all items have the same height)
+	const getItemLayout = (_: any, index: number) => ({
+		length: scale(82), // Adjust this to the actual item height
+		offset: scale(82) * index,
+		index,
+	});
 
-		return (
-			<SectionList
-				ref={ref}
-				// contentContainerStyle={styles.listContainer}
-				sections={sections}
-				ItemSeparatorComponent={() => (
-					<View style={styles.itemSeparator} />
-				)}
-				renderSectionHeader={({ section: { title } }) => (
-					<View style={styles.separatorHeaderContainer}>
-						<Text style={styles.separatorHeader}>{title}</Text>
-					</View>
-				)}
-				initialNumToRender={30}
-				getItemLayout={getItemLayout}
-				renderItem={({ item, index }) => {
-					const isFirst = index === 0;
-					const isLast = index === data.length - 1;
-					const itemExist = exists(item);
+	const renderItemCallback = useMemo(
+		() =>
+			({ item, index }) => {
+				const isFirst = index === 0;
+				const isLast = index === data.length - 1;
+				const itemExist = exists(item);
 
-					return (
-						<View
-							style={[
-								isFirst && styles.firstItem,
-								isLast && styles.lastItem,
-							]}
-						>
-							<SwipeItem
-								onDelete={() => onRemove(item)}
-								disableAction={itemExist}
-								onCloseAction={() => setCurrentOpenItem(null)}
-								onOpenAction={() =>
-									setCurrentOpenItem({ ...item })
-								}
-								isOpen={
-									item.identifier ===
-									currentOpenItem?.identifier
-								}
-								item={
-									<TouchableWithoutFeedback
-										onPress={() => setCurrentOpenItem(null)}
+				return (
+					<View
+						style={[
+							isFirst && styles.firstItem,
+							isLast && styles.lastItem,
+						]}
+					>
+						<SwipeItem
+							onDelete={() => onRemove(item)}
+							disableAction={itemExist}
+							onCloseAction={() => setCurrentOpenItem(null)}
+							onOpenAction={() => setCurrentOpenItem({ ...item })}
+							isOpen={
+								item.identifier === currentOpenItem?.identifier
+							}
+							item={
+								<TouchableWithoutFeedback
+									onPress={() => setCurrentOpenItem(null)}
+								>
+									<View
+										style={[
+											styles.item,
+											containerStyle,
+											isFirst && styles.firstItemBorder,
+											isLast && styles.lastItemBorder,
+										]}
 									>
-										<View
-											style={[
-												styles.item,
-												containerStyle,
-												isFirst &&
-													styles.firstItemBorder,
-												isLast && styles.lastItemBorder,
-											]}
-										>
-											<View>
-												{itemExist ? (
-													<RemoveCircle
-														height={editButtonSize}
-														width={editButtonSize}
-														onPress={() =>
-															setCurrentOpenItem(
-																item,
-															)
-														}
-													/>
-												) : (
-													<AddCircle
-														onPress={() =>
-															onAdd(item)
-														}
-														height={editButtonSize}
-														width={editButtonSize}
-													/>
-												)}
-											</View>
-											{renderItem(item)}
+										<View>
+											{itemExist ? (
+												<RemoveCircle
+													height={editButtonSize}
+													width={editButtonSize}
+													onPress={() =>
+														setCurrentOpenItem(item)
+													}
+												/>
+											) : (
+												<AddCircle
+													onPress={() => onAdd(item)}
+													height={editButtonSize}
+													width={editButtonSize}
+												/>
+											)}
 										</View>
-									</TouchableWithoutFeedback>
-								}
-							/>
-						</View>
-					);
-				}}
-				keyExtractor={(item) => item.identifier}
-			/>
-		);
-	},
-);
+										{renderItem(item)}
+									</View>
+								</TouchableWithoutFeedback>
+							}
+						/>
+					</View>
+				);
+			},
+		[sections, currentOpenItem, exists],
+	);
+	return (
+		<SectionList
+			initialNumToRender={15}
+			maxToRenderPerBatch={60}
+			windowSize={100}
+			stickySectionHeadersEnabled={true}
+			sections={sections}
+			ItemSeparatorComponent={() => <View style={styles.itemSeparator} />}
+			renderSectionHeader={({ section: { title } }) => (
+				<View style={styles.separatorHeaderContainer}>
+					<Text style={styles.separatorHeader}>{title}</Text>
+				</View>
+			)}
+			getItemLayout={getItemLayout}
+			renderItem={renderItemCallback}
+			keyExtractor={(item) => item.identifier}
+		/>
+	);
+}
 
 const styles = ScaledSheet.create({
 	separatorHeaderContainer: {
